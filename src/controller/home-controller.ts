17e091e9ec5380abf8af4a4db8b870677af23959
chenlong <903254852@qq.com>
2019-11-23 16:34:00 +0800

    odify

--- src/controller/home-controller.ts
+++ src/controller/home-controller.ts
@@ -1,13 +1,33 @@
+import * as ayc from 'async';
+
 const sleep = t => {
   const time = Date.now() + t * 1000;
   while (Date.now() < time) {}
 };
 
+var q = ayc.queue(function(task, callback) {
+  console.log('进入');
+  sleep(3);
+  console.log('success');
+  callback();
+}, 3);
+
+q.drain(function() {
+  console.log('所有任务处理完');
+});
+
+q.error(function(err, task) {
+  console.error(err);
+});
+
+let count = 0;
+
 export default class HomeController {
   static async hello(ctx) {
-    console.log('进入');
-    sleep(3);
-    console.log('success');
+    count++;
+    q.push({ name: '任务' + count }, err => {
+      console.log('完成处理:' + count);
+    });
     ctx.body = {
       state: 'success'
     };

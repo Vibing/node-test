f338cbc5489de4ab1cd8a213304a5877eab94e04
chenlong <903254852@qq.com>
2019-11-23 15:53:00 +0800

    modify

--- src/controller/home-controller.ts
+++ src/controller/home-controller.ts
@@ -1,7 +1,15 @@
-import HomeService from '../service/home-service';
+const sleep = t => {
+  const time = Date.now() + t * 1000;
+  while (Date.now() < time) {}
+};
 
 export default class HomeController {
   static async hello(ctx) {
-    ctx.body = await HomeService.hello();
+    console.log('进入');
+    sleep(3);
+    console.log('success');
+    ctx.body = {
+      state: 'success'
+    };
   }
 }
